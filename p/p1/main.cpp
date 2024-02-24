7ab40950bdf38c0f6b0e51643c35acc5f394ef0b
CarlDalebout <cadalebout1@couger.ciss.edu>
2024-02-23 18:59:30 -0600

    finished p01

--- p/p1/main.cpp
+++ p/p1/main.cpp
@@ -21,17 +21,15 @@ std::string dec_to_binary(uint32_t x, int bits)
 
 struct Block
 {
-    Block(bool free = true, std::string input = "00000000", uint64_t add = 0, uint32_t t = 0, uint32_t s = 0, uint32_t sh = 0)
-    :available(free), hex(input), address(add), tag(t), set(s), shift(sh)
+    Block(uint32_t t = 0, uint32_t s = 0, uint32_t sh = 0)
+    :tag(t), set(s), shift(sh)
     {}
 
-    inline bool operator==(Block a)
+    inline 
+    bool operator==(Block x)
     {
-        if (a.hex == hex &&
-            a.address == address &&
-            a.tag == tag &&
-            a.set == set &&
-            a.shift == shift)
+        if (x.tag == tag &&
+            x.set == set )
         {
             return true;
         }
@@ -39,166 +37,159 @@ struct Block
             return false;
     }
 
-    bool available;
-    std::string hex;
+    inline 
+    void print(int tag_bits_, int set_bits_, int shift_bits_)
+    {
+        std::cout << '|' << dec_to_binary(tag,   tag_bits_)     
+                  << '|' << dec_to_binary(set,   set_bits_)     
+                  << '|' << dec_to_binary(shift, shift_bits_) << "|\n";
+    }
+
     u_int64_t address;
     u_int32_t tag;
     u_int32_t set;
     u_int32_t shift;
 };
 
-
-
 class Cache
 {
-    // Number of blocks in main memory = (2^(s+w))/(2^w) = 2^s s = 9
-    // Block size = line size (64 bytes) = 2^w words of bytes w = 6
-    // Address length = (s + w)bits
-    // Number of addressable units = 2^(s+w)words or bytes
-    //  2^6 * 2^3 = 2^9 
-    // Number of lines in set = k
-    // Number of sets = v = 2^d
-    // Number of lines in cache = m(512) = kv = k * 2^d
-    // Size of cache = k * 2^(d+w)words or bytes
-    // Size of tag = (s - d)bits
-
-    // P.A. = bits
-
 public:
-    Cache(int bits_for_memory, int cache_size/*in bytes*/, int block_size/*in bytes*/, int k_associativity = 2, int victims_cache = 2)
-     :memory_size_(bits_for_memory), cache_size_(cache_size), block_size_(block_size), k_way_set_association_(k_associativity), victims_cache_(victims_cache),
-        pa_bits_(bits_for_memory), shift_bits_(std::log2(block_size)), 
-        number_of_cache_lines_(cache_size/block_size)
+    Cache(int bits_for_memory, int cache_size/*in bytes*/, int block_size/*in bytes*/, int k_associativity = 2, int victim_cache = 2)
+    :misses_(0), pa_bits_(bits_for_memory),shift_bits_(std::log2(block_size)), 
+     total_lines_(cache_size/block_size), victim_cache_size_(victim_cache)
     {
-        set_bits_ = std::log2(number_of_cache_lines_/k_associativity);
-        tag_bits_ = memory_size_ - (set_bits_ + shift_bits_);
-        blocks_list_ = std::vector<std::vector<Block>>(std::pow(2,set_bits_), std::vector<Block> ((number_of_cache_lines_ / std::pow(2,set_bits_)), Block()));
-        victims_list_ = std::vector<Block> (victims_cache_, Block());
+        set_size_ = total_lines_/k_associativity;
+        set_bits_ = std::log2(set_size_);
+        tag_bits_ = bits_for_memory - (set_bits_ + shift_bits_);
+        cache_ = std::vector<std::vector<Block>>(std::pow(2,set_bits_), std::vector<Block>());
+        victim_cache_ = std::vector<Block>();
     }
 
     inline
     void print_info()
     {
-        std::cout << "Tag: " << tag_bits_ << " set: " << set_bits_ << " shift: " << shift_bits_ << "\nBlocks: " << number_of_cache_lines_ << std::endl;
-    }
-
-    void push_top_victum_list(Block x)
-    {
-        for(int i = 0; i < victims_cache_; ++i)
-        {
-            if(victims_list_[i].available)
-            {
-                i++;
-            }
-            else
-            {
-
-            }
-        }
-        if(index < victims_cache_)
-        {
-            if(victims_list_[index].available || x == victims_list_[index])
-            {
-                // std::cout << "found empty / matching block" << std::endl;
-                victims_list_[index] = x;
-                victims_list_[index].available = false;
-                return;
-            }
-            else
-            {
-                // std::cout << "writing over index: " << index << std::endl;
-                Block temp = victims_list_[index];
-                victims_list_[index] = x;
-                victims_list_[index].available = false;
-                // std::cout << "calling push_top_block_list(temp, set, " << index+1 << ')' << std::endl;
-                push_top_victum_list(temp);
-            }
-        }
-    }
-
-
-    void push_top_block_list(Block x, int set, int index = 0)
-    {
-        // std::cout << "in push_top_block_list" << std::endl;
-        if(index < number_of_cache_lines_/std::pow(2, set_bits_))
-        {
-            if(blocks_list_[set][index].available || x == blocks_list_[set][index])
-            {
-                // std::cout << "found empty / matching block" << std::endl;
-                blocks_list_[set][index] = x;
-                blocks_list_[set][index].available = false;
-                return;
-            }
-            else
-            {
-                // std::cout << "writing over index: " << index << std::endl;
-                Block temp = blocks_list_[set][index];
-                blocks_list_[set][index] = x;
-                blocks_list_[set][index].available = false;
-                // std::cout << "calling push_top_block_list(temp, set, " << index+1 << ')' << std::endl;
-                push_top_block_list(temp, set, index+1);
-            }
-        }
-        else
-        {
-            // std::cout << "pusing to victum_cache" << std::endl;
-            Block temp = blocks_list_[set][index-1];
-            push_top_victum_list(temp);
-        }
+        std::cout << "Tag: " << tag_bits_ << " set: " << set_bits_ << " shift: " << shift_bits_ << "\nBlocks: " << total_lines_ << std::endl;
     }
 
     void print_cache()
     {
+        Block blank(0, 0, 0);
+        std::cout << misses_ << std::endl;
         std::cout << std::setfill('-') << std::setw(64) << ' ' << std::endl;
         for(int i = 0; i < (std::pow(2, set_bits_)); ++i)
         {   
-            for(int j = 0; j < (number_of_cache_lines_/std::pow(2,set_bits_)); ++j)
-            {
-                std::cout << '|' << (blocks_list_[i][j].available ? '1' : '0')             << '|' 
-                                 << dec_to_binary(blocks_list_[i][j].tag, tag_bits_)     << '|' 
-                                 << dec_to_binary(blocks_list_[i][j].set, set_bits_)     << '|'
-                                 << dec_to_binary(blocks_list_[i][j].shift, shift_bits_) << "|\n";
+            for(int j = 0; j < (total_lines_/set_size_); ++j)
+            {   
+                if(j < cache_[i].size())
+                    cache_[i][j].print(tag_bits_, set_bits_, shift_bits_);
+                else
+                    blank.print(tag_bits_, set_bits_, shift_bits_);
             }
             std::cout << std::setfill('-') << std::setw(64)  << "^" << i << std::endl;
         }
         std::cout << std::endl << std::setw(64) << std::left << "Victims_cache: " << std::endl;
-        for(int i = 0; i < victims_cache_; ++i)
+        for(int i = 0; i < victim_cache_size_; ++i)
         {   
-            std::cout << '|' << (victims_list_[i].available ? '1' : '0')           << '|' 
-                             << dec_to_binary(victims_list_[i].tag, tag_bits_)     << '|' 
-                             << dec_to_binary(victims_list_[i].set, set_bits_)     << '|'
-                             << dec_to_binary(victims_list_[i].shift, shift_bits_) << "|\n";
-    
+            if(i < victim_cache_.size())
+                victim_cache_[i].print(tag_bits_, set_bits_, shift_bits_);
+            else
+                blank.print(tag_bits_, set_bits_, shift_bits_);
         }
         std::cout << std::setfill('-') << std::setw(64) << ' ' << std::endl;
+        std::cout << "to load next address hit (enter), to skip printing n addresses type (skip_), to finish the file type (finish_)\n";
+    }
+
+    void push_victum_cache(Block x)
+    {
+        if(victim_cache_.size() < victim_cache_size_)
+        {
+            victim_cache_.insert(victim_cache_.begin(), x);
+        }
+        else
+        {
+            victim_cache_.erase(victim_cache_.begin());
+            victim_cache_.insert(victim_cache_.begin(), x);
+        }
+    }
+
+    bool check_victum_cache(Block x)
+    {
+        for(int i = 0; i < victim_cache_.size(); ++i)
+        {
+            if(x == victim_cache_[i])
+            {
+
+                if(cache_[x.set].size() < set_size_)
+                {
+                    cache_[x.set].push_back(x);
+                    victim_cache_.erase(victim_cache_.begin()+i);
+                }
+                else
+                {
+                    cache_[x.set].erase(cache_[x.set].begin());
+                    cache_[x.set].push_back(x);
+                    victim_cache_.erase(victim_cache_.begin()+i);
+                }
+                return true;
+            }
+        }
+        return false;
+    }
+
+    void check_cache(Block x)
+    {
+        for(int i = 0; i < cache_[x.set].size(); ++i)
+        {
+            if(x == cache_[x.set][i])
+            {
+                // std::cout << "hit set:" << x.set << "index: " << i << std::endl;
+                cache_[x.set].erase(cache_[x.set].begin()+i);
+                cache_[x.set].push_back(x);
+                return;
+            }
+        }
+
+        if(check_victum_cache(x))
+        {    
+            return;
+        }
+        else
+        {
+            misses_++;
+            if(cache_[x.set].size() < set_size_)
+            {
+                cache_[x.set].push_back(x);
+            }
+            else
+            {
+                push_victum_cache(cache_[x.set][0]);
+                cache_[x.set].erase(cache_[x.set].begin());
+                cache_[x.set].push_back(x);
+            }
+        }
     }
 
 // private:
-    int memory_size_; // in bits
-    int cache_size_;  // in bytes
-    int block_size_;  // in bytes
-    int k_way_set_association_; // number of associations
-    int victims_cache_; // number available
+int misses_;
 
-    int pa_bits_;
-    int tag_bits_;
-    int set_bits_;
-    int shift_bits_;
-    int number_of_cache_lines_;
-    std::vector<std::vector<Block>> blocks_list_;
-    std::vector<Block> victims_list_;
+int pa_bits_;
+int tag_bits_;
+int set_bits_;
+int set_size_;
+int shift_bits_;
+int total_lines_;
+int victim_cache_size_;
+
+std::vector<std::vector<Block>> cache_;
+std::vector<Block> victim_cache_;
+
 };
-
-
 
 Block get_block(std::string temp, Cache cache)
 {
     Block ret;
     temp.erase(0, 2);
     uint32_t temp_int = std::stoi(temp,nullptr,16);
-    ret.available = false;
-    ret.hex = temp;
-    ret.address = temp_int;
     ret.tag  = temp_int >> (cache.set_bits_ + cache.shift_bits_);
     ret.set  = ((temp_int >> cache.shift_bits_) << (cache.tag_bits_ + cache.shift_bits_)) >> ((cache.tag_bits_ + cache.shift_bits_)) ; // wrong
     ret.shift = (temp_int << (cache.pa_bits_ - cache.shift_bits_)) >> (cache.pa_bits_ - cache.shift_bits_);
@@ -208,24 +199,25 @@ Block get_block(std::string temp, Cache cache)
 
 void print_bits(Block temp, Cache cache)
 {
-    std::cout << std::setw(0) << temp.hex      << std::setfill(' ') << std::setw(14) << std::left << ' ' << "    PA: " << dec_to_binary(temp.address, cache.pa_bits_) << std::endl;
     std::cout << std::setw(20) << std::left << ' ' << "   tag: " << dec_to_binary(temp.tag, cache.tag_bits_) << std::endl;
     std::cout << std::setw(20) << std::left << ' ' << "   set:  " << dec_to_binary(temp.set, cache.set_bits_) << std::endl;
     std::cout << std::setw(20) << std::left << ' ' << " shift:  " << dec_to_binary(temp.shift, cache.shift_bits_) << std::endl;
     std::cout << std::setw(64) << std::setfill('-') << " " << std::endl << std::endl;
 }
 
-int main ()
+int main()
 {
-    Cache cache( 32, 512, 64);
-    // Cache cache( 32, 8000, 16);
+
+    Cache cache( 32, 512, 64, 2, 2);
     cache.print_info();
-    cache.print_cache();
+    std::cout << cache.victim_cache_.size() << std::endl;
+    // cache.print_cache();
     
     std::fstream fsread;
     fsread.open("data.txt");
     
     std::string temp = "";
+    bool finish = false;
     int skip = -1;
     int misses = 0;
     while(!fsread.eof())
@@ -234,63 +226,33 @@ int main ()
         std::getline(fsread, temp);
         Block temp_block = get_block(temp, cache);
         
-        // print_bits(temp_block, cache);
-        
-        for(int i = 0; i < cache.number_of_cache_lines_/std::pow(2, cache.set_bits_); ++i)
-        {
-            // std::cout << "checking block_list set: " << temp_block.set << ' ' << i << std::endl;
-            if(temp_block == cache.blocks_list_[temp_block.set][i])
+        cache.check_cache(temp_block);
+
+        if(!finish) 
+        {   
+            std::cout << temp.erase(0, 2) << '\t' << dec_to_binary(std::stoi(temp, nullptr, 16), cache.pa_bits_) << std::endl;
+            print_bits(temp_block, cache);
+            cache.print_cache();
+            
+            if(skip < 0)
             {
-                found = true;
-                // std::cout << "case 1 pushing to cache.set: " << temp_block.set << std::endl;
-                cache.blocks_list_[temp_block.set][i].available = true;
-                cache.push_top_block_list(temp_block, temp_block.set, 0);
-                break;
+                std::getline(std::cin, temp);
+                if(temp == "print_cache")
+                {
+                }
+                else if(temp == "skip_")
+                {
+                    std::cout << "how many lines do you want to skip: ";
+                    int amount = 0;
+                    std::cin >> skip;
+                }
+                else if(temp == "finish_")
+                    finish = true;
             }
+            skip--;
         }
-        if(found)
-        {
-            continue;
-        }
-        for(int i = 0; i < cache.victims_cache_; ++i)
-        {
-            // std::cout << "checking victum_cache: " << i << std::endl;
-            if(temp_block == cache.victims_list_[i])
-            {
-                cache.victims_list_[i].available = true;
-                // std::cout << "case 2 pushing to cache.set: " << temp_block.set << std::endl;
-                cache.push_top_block_list(temp_block, temp_block.set, 0);
-                found = true;
-            }
-        }
-        if(found)
-        {
-            continue;
-        }
-        else
-        {
-            misses++;
-            std::cout << "cache miss total: " << misses << std::endl;
-            // std::cout << "case 3 pushing to cache.set: " << temp_block.set << std::endl;
-            cache.push_top_block_list(temp_block, temp_block.set, 0);
-        }
-        // if(skip < 0)
-        // {
-        //     std::getline(std::cin, temp);
-        //     if(temp == "print_cache")
-        //     {
-        //         cache.print_cache();
-        //     }
-        //     else if(temp == "skip_")
-        //     {
-        //         std::cout << "how many lines do you want to skip: ";
-        //         int amount = 0;
-        //         std::cin >> skip;
-        //     }
-        // }
-        // skip--;
     }
+    std::cout << cache.misses_ << std::endl;
     fsread.close();
-    
     return 0;
 }
